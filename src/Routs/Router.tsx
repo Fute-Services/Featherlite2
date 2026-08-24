1c648b522949f2e1830c31346b6121075e632be0
Gautam <webdev1@futeservices.com>
2026-08-24 12:21:38 +0530

    Add VR tour embed to Location page and enable /location route

--- src/Routs/Router.tsx
+++ src/Routs/Router.tsx
@@ -4,7 +4,7 @@ import Header from '../Components/Header/Header'
 import Navbar from '../Components/Navbar/Navbar'
 import IntroCurtain from '../Components/Intro/IntroCurtain'
 import Homepage from '../Pages/Homepage'
-// import Location from '../Pages/Location'
+import Location from '../Pages/Location'
 
 // Everything except the homepage is code-split: each route's JS (and the
 // heavy images/video/three.js it pulls in) only downloads when visited.
@@ -49,7 +49,7 @@ const Router = () => {
           {/* Routes WITH Navbar */}
           <Route element={<MainLayout />}>
             <Route path="/" element={<Homepage />} />
-            {/* <Route path="/location" element={<Location />} /> */}
+            <Route path="/location" element={<Location />} />
             <Route path="/amenities" element={<Amenities />} />
             <Route path="/media" element={<Media />} />
             <Route path="/certifications" element={<CertificationsPage />} />
